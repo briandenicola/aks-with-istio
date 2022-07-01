0fe516dcceae9e0acefa2a2d8097c44d3f4e5b91
briandenicola <brian@briandenicola.com>
2022-07-01 10:05:44 -0500

    Updated kubernetes providers

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -31,12 +31,16 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = [
       "get-token", 
+      "--environment",
+      "AzurePublicCloud",
       "--server-id", 
-      "6dae42f8-4368-4678-94ff-3960e28e3630", 
+      "6dae42f8-4368-4678-94ff-3960e28e3630",
+      "--client-id",
+      "80faf920-1908-4b52-b5ef-a8e7bedfc67a",
       "--tenant-id",
       data.azurerm_client_config.current.tenant_id,
       "--login", 
-      "msi",
+      "azurecli",
     ]
     command     = "kubelogin"
   }
