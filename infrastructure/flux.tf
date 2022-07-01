cd77139715a97955c6f6565204b8a6e15123ce65
briandenicola <brian@briandenicola.com>
2022-07-01 12:56:09 -0500

    Working example with fluxconfiguration

--- infrastructure/flux.tf
+++ infrastructure/flux.tf
@@ -15,39 +15,39 @@ resource "azapi_resource" "flux_install" {
     })
 }
 
-#resource "azapi_resource" "flux_config" {
-#    depends_on = [
-#        azapi_update_resource.flux_install
-#    ]
+resource "azapi_resource" "flux_config" {
+    depends_on = [
+        azapi_resource.flux_install
+    ]
 
-#    type        = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
-#    resource_id = azurerm_kubernetes_cluster.this.id
+    type        = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
+    name        = "flux-learning"
+    parent_id   = azurerm_kubernetes_cluster.this.id
 
-#    body = jsonencode({})
-    #  properties: {
-    #    scope: 'cluster'
-    #    namespace: 'gitops-demo'
-    #    sourceKind: 'GitRepository'
-    #    suspend: false
-    #    gitRepository: {
-    #      url: 'https://github.com/fluxcd/flux2-kustomize-helm-example'
-    #      timeoutInSeconds: 600
-    #      syncIntervalInSeconds: 600
-    #      repositoryRef: {
-    #        branch: 'main'
-    #      }
-    #
-    #    }
-    #    kustomizations: {
-    #      infra: {
-    #        path: './infrastructure'
-    #        dependsOn: []
-    #        timeoutInSeconds: 600
-    #        syncIntervalInSeconds: 600
-    #        validation: 'none'
-    #        prune: true
-    #      }
-    #    }
-    #  }
-    #}
-#}
+    body = jsonencode({
+        properties: {
+            scope         = "cluster"
+            namespace     = "flux-system"
+            sourceKind    = "GitRepository"
+            suspend       = false
+            gitRepository = {
+                url                     = "https://github.com/briandenicola/flux-learning"
+                timeoutInSeconds        = 600
+                syncIntervalInSeconds   = 300
+                repositoryRef           = {
+                    branch = "main"
+                }
+            }
+            kustomizations: {
+                aks = {
+                    path                    = "./clusters/aks"
+                    dependsOn               = []
+                    timeoutInSeconds        = 600
+                    syncIntervalInSeconds   = 300
+                    retryIntervalInSeconds  = 300
+                    prune                   = true
+                }
+            }
+        }
+    })
+}
