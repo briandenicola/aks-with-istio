b062be8660bc9f5bcae6551e16a22d8c77d159aa
briandenicola <brian@briandenicola.com>
2022-07-01 15:31:52 -0500

    Formatting

--- infrastructure/flux.tf
+++ infrastructure/flux.tf
@@ -1,53 +1,53 @@
 resource "azapi_resource" "flux_install" {
-    depends_on = [
-        azurerm_kubernetes_cluster.this
-    ]
+  depends_on = [
+    azurerm_kubernetes_cluster.this
+  ]
 
-    type = "Microsoft.KubernetesConfiguration/extensions@2021-09-01"
-    name = "flux"
-    parent_id = azurerm_kubernetes_cluster.this.id
+  type      = "Microsoft.KubernetesConfiguration/extensions@2021-09-01"
+  name      = "flux"
+  parent_id = azurerm_kubernetes_cluster.this.id
 
-    body = jsonencode({
-        properties = {
-            extensionType = "microsoft.flux"
-            autoUpgradeMinorVersion = true
-        }
-    })
+  body = jsonencode({
+    properties = {
+      extensionType           = "microsoft.flux"
+      autoUpgradeMinorVersion = true
+    }
+  })
 }
 
 resource "azapi_resource" "flux_config" {
-    depends_on = [
-        azapi_resource.flux_install
-    ]
+  depends_on = [
+    azapi_resource.flux_install
+  ]
 
-    type        = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
-    name        = "aks-flux-extension"
-    parent_id   = azurerm_kubernetes_cluster.this.id
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
+  name      = "aks-flux-extension"
+  parent_id = azurerm_kubernetes_cluster.this.id
 
-    body = jsonencode({
-        properties: {
-            scope         = "cluster"
-            namespace     = "flux-system"
-            sourceKind    = "GitRepository"
-            suspend       = false
-            gitRepository = {
-                url                     = "https://github.com/briandenicola/aks-flux-extension"
-                timeoutInSeconds        = 600
-                syncIntervalInSeconds   = 300
-                repositoryRef           = {
-                    branch = "main"
-                }
-            }
-            kustomizations: {
-                aks = {
-                    path                    = "./clusters/aks"
-                    dependsOn               = []
-                    timeoutInSeconds        = 600
-                    syncIntervalInSeconds   = 300
-                    retryIntervalInSeconds  = 300
-                    prune                   = true
-                }
-            }
+  body = jsonencode({
+    properties : {
+      scope      = "cluster"
+      namespace  = "flux-system"
+      sourceKind = "GitRepository"
+      suspend    = false
+      gitRepository = {
+        url                   = "https://github.com/briandenicola/aks-flux-extension"
+        timeoutInSeconds      = 600
+        syncIntervalInSeconds = 300
+        repositoryRef = {
+          branch = "main"
         }
-    })
+      }
+      kustomizations : {
+        aks = {
+          path                   = "./clusters/aks"
+          dependsOn              = []
+          timeoutInSeconds       = 600
+          syncIntervalInSeconds  = 300
+          retryIntervalInSeconds = 300
+          prune                  = true
+        }
+      }
+    }
+  })
 }
