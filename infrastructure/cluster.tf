9a78c8fc6e59ffe1f9e051b117a39c0a058dfd6a
Brian <brian@briandenicola.com>
2022-10-13 15:11:14 +0000

    Moved OSM to false

--- infrastructure/cluster.tf
+++ infrastructure/cluster.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "this" {
   dns_prefix                      = local.aks_name
   sku_tier                        = "Paid"
   oidc_issuer_enabled             = true
-  open_service_mesh_enabled       = true
+  open_service_mesh_enabled       = false
   azure_policy_enabled            = true
   api_server_authorized_ip_ranges = ["${chomp(data.http.myip.body)}/32"]
 
