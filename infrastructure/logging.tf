52fe2be2ee9cf13f5c96611ece4cc7feedbf5b4d
briandenicola <brian@briandenicola.com>
2023-04-05 14:29:43 -0500

    Reverting msi_auth_for_monitoring_enabled

--- infrastructure/logging.tf
+++ infrastructure/logging.tf
@@ -2,7 +2,7 @@ resource "azurerm_log_analytics_workspace" "this" {
   name                          = "${local.resource_name}-logs"
   location                      = azurerm_resource_group.this.location
   resource_group_name           = azurerm_resource_group.this.name
-  local_authentication_disabled = true
+  local_authentication_disabled = false
   sku                           = "PerGB2018"
   daily_quota_gb                = 0.5
 }
@@ -26,7 +26,7 @@ resource "azurerm_application_insights" "this" {
   resource_group_name           = azurerm_resource_group.this.name
   workspace_id                  = azurerm_log_analytics_workspace.this.id
   application_type              = "web"
-  local_authentication_disabled = true
+  local_authentication_disabled = false
 }
 
 locals {
