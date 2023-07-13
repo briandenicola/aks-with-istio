d3a74382e885c724955697554d6c8df7ca4360f7
briandenicola <brian@briandenicola.com>
2023-07-13 14:48:23 -0500

    Bug fix on api version

--- infrastructure/cluster-post-config.tf
+++ infrastructure/cluster-post-config.tf
@@ -3,7 +3,7 @@ resource "azapi_update_resource" "istio_ingressgateway" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2023-03-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2023-05-02-preview"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
