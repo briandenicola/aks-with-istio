26b885c12ac1ed0d34ba3e11475c81ac8bb23039
briandenicola <brian@briandenicola.com>
2024-03-19 09:22:37 -0500

    Added cost analysis

--- infrastructure/cluster-post-config.tf
+++ infrastructure/cluster-post-config.tf
@@ -3,11 +3,16 @@ resource "azapi_update_resource" "cluster_updates" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2023-05-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2024-01-02-preview"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
     properties = {
+      metricsProfile = {
+        costAnalysis = {
+          enabled = true
+        }
+      }
       networkProfile = {
         monitoring = {
           enabled = true
