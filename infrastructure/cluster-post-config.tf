12d0fdee1baea4f681545a94fb749a8cd349c20e
briandenicola <brian@briandenicola.com>
2024-04-09 14:50:45 -0500

    🏐 Tue Apr  9 14:50:45 CDT 2024

--- infrastructure/cluster-post-config.tf
+++ infrastructure/cluster-post-config.tf
@@ -3,7 +3,7 @@ resource "azapi_update_resource" "cluster_updates" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2023-11-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2024-01-02-preview"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
